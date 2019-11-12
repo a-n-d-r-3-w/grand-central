55021b4cd97df0ab163d3c5ea95228af80ce23cd
andr3w <liu.anray+github@gmail.com>
2019-11-11 23:16:32 -0500

    Delete comment

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,6 @@ const App: React.FC = () => {
             </li>
           </ul>
         </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/about-others">
             <AboutOthers />
