be9d30b800540d4d2f49295b13fa3e5633ba31ee
andr3w <liu.anray+github@gmail.com>
2019-11-13 18:08:15 -0500

    Add Redux devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { grandCentralReducer } from './grandCentralReducer';
 
-const store = createStore(grandCentralReducer);
+const store = createStore(
+  grandCentralReducer,
+  {},
+  composeWithDevTools()
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
