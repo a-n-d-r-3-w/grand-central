6cf1b6be3890934e3ac5b104e287a26bd4db0c82
andr3w <liu.anray+github@gmail.com>
2019-11-24 14:13:22 -0500

    Install body-parser; implement POST person

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const connectRunClose = require('./connectRunClose');
 const shortid = require('shortid');
@@ -6,6 +7,8 @@ const HttpStatus = require('http-status-codes');
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.use(express.static('build'));
 
@@ -29,6 +32,34 @@ app.get('/api/accounts/:accountId', async (req, res) => {
     return;
   }
   res.status(HttpStatus.OK).json(account);
+});
+
+app.post('/api/accounts/:accountId/people', async (req, res) => {
+  if (!req.body) {
+    res.send(HttpStatus.BAD_REQUEST, 'Name is missing.');
+    return;
+  }
+
+  const { name } = req.body;
+  if (name.trim().length === 0) {
+    res.send(HttpStatus.BAD_REQUEST, 'Name is empty.');
+    return;
+  }
+
+  const { accountId } = req.params;
+  const account = await connectRunClose('accounts', accounts => accounts.findOne({ accountId }));
+  const { people } = account;
+  const personId = shortid.generate();
+  const person = {
+    personId,
+    name: req.body.name,
+    info: ''
+  };
+  people.push(person);
+  await connectRunClose('accounts', accounts => accounts.updateOne(
+    { accountId },
+    { $set: { people } }));
+  res.send(HttpStatus.CREATED, person);
 });
 
 app.get('/*', (req, res) => {
