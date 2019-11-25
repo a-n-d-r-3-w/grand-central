8f6df4934c81c461470ced68ad847be05b0594d6
andr3w <liu.anray+github@gmail.com>
2019-11-24 21:16:45 -0500

    Extract people utils

--- routers/people.js
+++ routers/people.js
@@ -1,91 +1,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const HttpStatus = require('http-status-codes');
-const shortid = require('shortid');
-const connectRunClose = require('../connectRunClose');
+const {
+  addPersonToAccount,
+  getPeople,
+  updatePerson
+} = require('./peopleUtils');
 
 const router = express.Router();
 router.use(bodyParser.json());
 
-const ACCOUNTS_COLLECTION_NAME = 'accounts';
-
-const getAccount = async accountId =>
-  await connectRunClose(ACCOUNTS_COLLECTION_NAME, accounts =>
-    accounts.findOne({ accountId })
-  );
-
-const setPeople = async (accountId, people) => {
-  await connectRunClose(ACCOUNTS_COLLECTION_NAME, accounts =>
-    accounts.updateOne({ accountId }, { $set: { people } })
-  );
-};
-
-// Create person
 router.post('/', async (req, res) => {
-  if (!req.body) {
-    res.send(HttpStatus.BAD_REQUEST, 'Name is missing.');
-    return;
-  }
-
-  const { name } = req.body;
-  if (name.trim().length === 0) {
-    res.send(HttpStatus.BAD_REQUEST, 'Name is empty.');
-    return;
-  }
-
-  const { accountId } = req.forwardedParams;
-  const account = await getAccount(accountId);
-  const { people } = account;
-  const personId = shortid.generate();
-  const person = {
-    personId,
-    name: req.body.name,
-    info: ''
-  };
-  people.push(person);
-  await setPeople(accountId, people);
-  res.send(HttpStatus.CREATED, person);
+  await addPersonToAccount(req.forwardedParams.accountId, req.body.name);
+  res.sendStatus(HttpStatus.CREATED);
 });
 
-const sortPeople = people => {
-  people.sort((person1, person2) => {
-    const name1 = person1.name.toLowerCase();
-    const name2 = person2.name.toLowerCase();
-    if (name1 < name2) {
-      return -1;
-    }
-    if (name1 > name2) {
-      return 1;
-    }
-    return 0;
-  });
-};
-
-// Get people
 router.get('/', async (req, res) => {
-  const { accountId } = req.forwardedParams;
-  const account = await getAccount(accountId);
-  if (account === null) {
-    res.send(HttpStatus.NOT_FOUND);
-    return;
-  }
-  const { people } = account;
-  sortPeople(people);
+  const people = await getPeople(req.forwardedParams.accountId);
   res.send(HttpStatus.OK, people);
 });
 
-// Modify person
 router.put('/:personId', async (req, res) => {
   const { accountId, personId } = req.forwardedParams;
-  const account = await getAccount(accountId);
-  if (account === null) {
-    res.send(HttpStatus.NOT_FOUND);
-    return;
-  }
-  const { people } = account;
-  const index = people.findIndex(person => person.personId === personId);
-  people[index] = { ...people[index], ...req.body };
-  await setPeople(accountId, people);
+  await updatePerson(accountId, personId, req.body);
   res.send(HttpStatus.NO_CONTENT);
 });
 
---
+++ routers/peopleUtils.js
@@ -0,0 +1,60 @@
+const shortid = require('shortid');
+const connectRunClose = require('../connectRunClose');
+
+const ACCOUNTS_COLLECTION_NAME = 'accounts';
+
+const getAccount = async accountId =>
+  await connectRunClose(ACCOUNTS_COLLECTION_NAME, accounts =>
+    accounts.findOne({ accountId })
+  );
+
+const sortPeople = people => {
+  people.sort((person1, person2) => {
+    const name1 = person1.name.toLowerCase();
+    const name2 = person2.name.toLowerCase();
+    if (name1 < name2) {
+      return -1;
+    }
+    if (name1 > name2) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+const getPeople = async accountId => {
+  const account = await getAccount(accountId);
+  const people = account.people;
+  sortPeople(people);
+  return people;
+};
+
+const setPeople = async (accountId, people) => {
+  await connectRunClose(ACCOUNTS_COLLECTION_NAME, accounts =>
+    accounts.updateOne({ accountId }, { $set: { people } })
+  );
+};
+
+const addPersonToAccount = async (accountId, name) => {
+  const person = {
+    personId: shortid.generate(),
+    name,
+    info: ''
+  };
+  const people = await getPeople(accountId);
+  people.push(person);
+  await setPeople(accountId, people);
+};
+
+const updatePerson = async (accountId, personId, newData) => {
+  const people = await getPeople(accountId);
+  const index = people.findIndex(person => person.personId === personId);
+  people[index] = { ...people[index], ...newData };
+  await setPeople(accountId, people);
+};
+
+module.exports = {
+  addPersonToAccount,
+  getPeople,
+  updatePerson
+};
