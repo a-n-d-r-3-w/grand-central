87b4c1ad9bc082f6ca850b2c5453441ab76abdb3
andr3w <liu.anray+github@gmail.com>
2019-11-24 22:39:01 -0500

    Remove list of accounts in UI

--- src/AboutOthers/aboutOthersReducer.js
+++ src/AboutOthers/aboutOthersReducer.js
@@ -1,22 +1,17 @@
 import { CREATE_ACCOUNT } from './aboutOthersActions';
 
 const initialState = {
-  accounts: []
+  accountId: null
 };
 
 export const aboutOthersReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_ACCOUNT:
-      const accounts = state.accounts.slice();
       if (!action.accountId) {
         throw new Error('accountId is falsy.');
       }
-      accounts.push({
-        accountId: action.accountId,
-        people: []
-      });
       return {
-        accounts
+        accountId: action.accountId
       };
     default:
       return state;
--- src/AboutOthers/index.jsx
+++ src/AboutOthers/index.jsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 import { createAccount } from './aboutOthersActions';
 
-function AboutOthers(props) {
+const AboutOthers = props => {
   const handleClickCreateAccount = () => {
     props.createAccount();
   };
@@ -18,21 +18,10 @@ function AboutOthers(props) {
       >
         Create account
       </Button>
-      {props.accounts.map(account => (
-        <div key={account.accountId}>{account.accountId}</div>
-      ))}
     </>
   );
-}
-
-AboutOthers.defaultProps = {
-  accounts: []
 };
-
-const mapStateToProps = state => ({
-  accounts: state.aboutOthersReducer.accounts
-});
 
 const mapDispatchToProps = { createAccount };
 
-export default connect(mapStateToProps, mapDispatchToProps)(AboutOthers);
+export default connect(null, mapDispatchToProps)(AboutOthers);
