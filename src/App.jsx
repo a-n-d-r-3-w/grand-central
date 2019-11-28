a31632905b18d4040b3415fd883642e08e5337e8
andr3w <liu.anray+github@gmail.com>
2019-11-27 20:20:58 -0500

    Remove material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
 import AboutOthers from './AboutOthers/index';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Switch>
-          <Route path="/about-others">
-            <AboutOthers />
-          </Route>
-          <Route path="/">
-            <GrandCentral />
-          </Route>
-        </Switch>
-      </Container>
+      <Switch>
+        <Route path="/about-others">
+          <AboutOthers />
+        </Route>
+        <Route path="/">
+          <GrandCentral />
+        </Route>
+      </Switch>
     </Router>
   );
 };
