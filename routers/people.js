b7e7a0798afcdb642f429ad9d5b3afaa7c884eed
andr3w <liu.anray+github@gmail.com>
2019-12-06 12:37:50 -0500

    Revert "Add Till test"
    This reverts commit e1264e07740c7bee6265d171919d480e7450e1e2.

--- routers/people.js
+++ routers/people.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const express = require('express');
 const HttpStatus = require('http-status-codes');
@@ -30,13 +29,6 @@ router.put('/:personId', async (req, res) => {
 
 router.delete('/:personId', async (req, res) => {
   await deletePerson(req.params.personId);
-  const tillUsername = 'bcb5bf8ead5b4a48ad16315ab5fc9a';
-  const tillApiKey = '5954c18031ec2b9d8b034320041da2c55e079a6a';
-  const tillUrl = `https://platform.tillmobile.com/api/send?username=${tillUsername}&api_key=${tillApiKey}`;
-  await axios.post(tillUrl, {
-    phone: ['857-928-6929'],
-    text: `Hello from Till on Grand Central! Deleted ${req.params.personId}.`
-  });
   res.sendStatus(HttpStatus.NO_CONTENT);
 });
 
