0e0c96dfe7517f9758fae4cd1e7dc66387b6b5f2
andr3w <liu.anray+github@gmail.com>
2019-12-06 15:34:54 -0500

    Implement email timer

--- server.js
+++ server.js
@@ -21,3 +21,35 @@ if (port == null || port === '') {
 }
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// Start quotes email timer
+const sendEmail = () => {
+  const helper = require('sendgrid').mail;
+  const from_email = new helper.Email('donotreply@quotes.com');
+  const to_email = new helper.Email('liu.anray@gmail.com');
+  const subject = 'Hello from Grand Central Quotes!';
+  const content = new helper.Content(
+    'text/plain',
+    'Being stuck is an honourable state and an essential part of improving thinking. - Thinking Mathematically'
+  );
+  const mail = new helper.Mail(from_email, subject, to_email, content);
+
+  const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  const request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+};
+
+const ONE_SECOND = 1000;
+const ONE_MINUTE = 60 * ONE_SECOND;
+const ONE_HOUR = 60 * ONE_MINUTE;
+
+setInterval(sendEmail, ONE_HOUR);
