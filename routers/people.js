21828e1b641b1afc6670851fed692e8170fc321d
andr3w <liu.anray+github@gmail.com>
2019-12-06 14:04:24 -0500

    Added dotenv; send email on delete person

--- routers/people.js
+++ routers/people.js
@@ -29,6 +29,27 @@ router.put('/:personId', async (req, res) => {
 
 router.delete('/:personId', async (req, res) => {
   await deletePerson(req.params.personId);
+
+  const helper = require('sendgrid').mail;
+  const from_email = new helper.Email('donotreply@example.com');
+  const to_email = new helper.Email('liu.anray@gmail.com');
+  const subject = 'Hello World from the SendGrid Node.js Library!';
+  const content = new helper.Content('text/plain', 'Hello, Email!');
+  const mail = new helper.Mail(from_email, subject, to_email, content);
+
+  const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  const request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+
   res.sendStatus(HttpStatus.NO_CONTENT);
 });
 
