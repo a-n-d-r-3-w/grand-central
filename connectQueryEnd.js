306153bf9347d538624d09027712faeef43e1cb8
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2021-04-19 22:35:41 -0400

    Use remote DB on remote server

--- connectQueryEnd.js
+++ connectQueryEnd.js
@@ -1,20 +1,20 @@
 const mariadb = require('mariadb');
 
 let pool;
-// if (process.env.NODE_ENV === 'production') {
-//   pool = mariadb.createPool({
-//     host: 'grandcentral.db',
-//     user: 'about_others',
-//     password: '8672E28DF16BF487A',
-//     connectionLimit: 5
-//   });
-// } else {
-pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'grand_central',
-  connectionLimit: 5
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+  pool = mariadb.createPool({
+    host: 'grandcentral.db',
+    user: 'about_others',
+    password: '8672E28DF16BF487A',
+    connectionLimit: 5
+  });
+} else {
+  pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'grand_central',
+    connectionLimit: 5
+  });
+}
 
 module.exports = sql => {
   return pool.getConnection()
