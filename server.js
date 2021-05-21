c5b321a73f1bac16cc2dc901ff54d4487f82f571
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2021-05-21 15:19:33 -0400

    Add rate limiter

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const serveIndex = require('serve-index')
 const path = require('path');
+const rateLimit = require("express-rate-limit");
 const people = require('./routers/people');
 const entries = require('./routers/entries');
 const login = require('./routers/login');
@@ -16,6 +17,16 @@ app.use('/blog', express.static('blog'), serveIndex('blog', {
   },
   icons: true
 }));
+
+// Rate limiter
+// Enable 'trust proxy' if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10
+});
+app.use("/api/", apiLimiter);
 
 app.use('/api/about-others/people', people);
 app.use('/api/ohlife/entries', entries);
