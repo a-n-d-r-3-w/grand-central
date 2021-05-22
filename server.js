50356994f9aa7b06196cd4a8f00926429a1a2ed1
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2021-05-22 17:09:49 -0400

    Increase allowed traffic

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/blog', express.static('blog'), serveIndex('blog', {
 // app.set('trust proxy', 1);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20 // limit each IP to 20 requests per windowMs
+  max: 100 // limit each IP to `max` requests per `windowMs`
 });
 app.use(limiter);
 
