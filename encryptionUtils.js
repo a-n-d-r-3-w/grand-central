3f7e33efe7fd92c037b4502f85c614fa34756cd7
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2021-06-04 18:59:31 -0400

    Add unit tests for encryption utils

--- encryptionUtils.js
+++ encryptionUtils.js
@@ -1,15 +1,13 @@
 const crypto = require('crypto');
 
-const encrypt = (plaintext, encryptionKey) => {
-    const iv = process.env.ENCRYPTION_INITIALIZATION_VECTOR;
+const encrypt = (plaintext, encryptionKey, iv = process.env.ENCRYPTION_INITIALIZATION_VECTOR) => {
     const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, iv);
     let encryptedText = cipher.update(plaintext, 'utf-8', 'hex');
     encryptedText += cipher.final('hex');
     return encryptedText;
 }
 
-const decrypt = (encryptedText, encryptionKey) => {
-    const iv = process.env.ENCRYPTION_INITIALIZATION_VECTOR;
+const decrypt = (encryptedText, encryptionKey, iv = process.env.ENCRYPTION_INITIALIZATION_VECTOR) => {
     const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, iv);
     let decryptedText = decipher.update(encryptedText, 'hex', 'utf-8');
     decryptedText += decipher.final('utf-8');
