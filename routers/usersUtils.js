a06d901341e500c28e38104626aa3d1a1571e400
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-11-05 10:35:30 -0400

    Store username and hashed password into database

--- routers/usersUtils.js
+++ routers/usersUtils.js
@@ -1,10 +1,14 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const connectQueryEnd = require('../connectQueryEnd');
 
 const createUser = async (username, password) => {
   const salt = await bcrypt.genSalt(saltRounds);
-  const hash = await bcrypt.hash(password, salt); // The hash includes the salt.
+  const hashedPassword = await bcrypt.hash(password, salt); // The hash includes the salt.
   // Store hash into DB
+  const sql = `INSERT INTO inclusive_web_apps.users (username, hashed_password) VALUES (?, ?);`;
+  const args = [username, hashedPassword];
+  return await connectQueryEnd(sql, args);
 };
 
 module.exports = {
