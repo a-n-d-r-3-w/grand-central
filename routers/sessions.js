ed4a241ea422493739df68ea277287fb59ecdbd1
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-11-06 18:00:32 -0500

    Store session in database and return session ID in cookie

--- routers/sessions.js
+++ routers/sessions.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const HttpStatus = require('http-status-codes');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const connectQueryEnd = require('../connectQueryEnd');
 
 const router = express.Router();
@@ -24,10 +25,20 @@ router.post('/', async (req, res) => {
   try {
     const { username, password } = req.body;
     await validateUsernameAndPassword(username, password);
+
     // Create session key.
+    const sessionId = crypto.randomBytes(32).toString('hex');
     // Store session in database.
-    // Generate session cookie.
+    const sql = `INSERT INTO inclusive_web_apps.sessions (sessionId, username) VALUES (?, ?);`;
+    const args = [sessionId, username];
+    await connectQueryEnd(sql, args);
     // Set response cookie.
+    res.cookie('sessionId', sessionId, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production'
+    });
+
     // Redirect user to their home page.
     res.send('Logged in.');
   } catch (error) {
